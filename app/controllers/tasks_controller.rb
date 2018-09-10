a82ee002503169e4289806db65e5ac6264916fac
test.ito <itotest110test@gmail.com>
2018-09-10 14:18:26 +0000

    modification of pointing parts

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :corrent_user, only: [:destroy]
+  before_action :corrent_user, only: [:show, :edit, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
